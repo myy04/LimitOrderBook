cbee471e9798c09225f3a07a4292b384159386a0
myy04 <mussabek.yerdaulet@gmail.com>
2026-08-23 15:41:17 +0500

    implemented OrderBook data structure in C++

---
+++ src/cpp/src/OrderBook.cpp
@@ -0,0 +1,30 @@
+#include ".././include/lob/OrderBook.h"
+
+OrderBook::OrderBook(): bids{}, asks{}, nodes{} {}
+
+void OrderBook::insert_order(std::shared_ptr<Order> order) noexcept {
+    auto& tree = (order->side == OrderSide::BUY) ? bids : asks;
+    if (tree.find(order->price) == tree.end()) tree[order->price] = {};
+    auto& list = tree[order->price];
+    list.push_back(order);
+}  
+
+void OrderBook::remove_order(std::shared_ptr<Order> order) noexcept {
+    if (nodes.find(order->order_id) == nodes.end()) return;
+    auto& tree = (order->side == OrderSide::BUY) ? bids : asks;
+    auto& list = tree[order->price];
+    auto& node = nodes[order->order_id];
+    list.erase(node);
+}
+
+std::shared_ptr<Order> OrderBook::peek_best_bid() {         
+    if (bids.empty()) throw "No bids are in the orderbook";
+    auto& list = bids.rbegin()->second;
+    return *list.begin();
+}
+
+std::shared_ptr<Order> OrderBook::peek_best_ask() { 
+    if (asks.empty()) throw "no asks are in the orderbook";
+    auto& list = asks.rbegin()->second;
+    return *list.begin();
+}
